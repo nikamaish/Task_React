e20caed4e9203261a551efe11d51e28ba07f2c8b
aish <aishnikam31@gmail.com>
2024-06-22 23:46:58 +0530

    :zap: styling to form

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import EmployeeList from './components/list/EmployeeList';
+import AddEmployee from './components/add/AddEmployee';
+import Navbar from './components/navbar/Navbar';
+import './App.css'
 
 const App = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const [employees, setEmployees] = useState([
+    { EmployeeId: '1', EmpName: 'John Doe', Department: 'IT', Salary: 50000 },
+    { EmployeeId: '2', EmpName: 'Jane Smith', Department: 'HR', Salary: 60000 },
+  ]);
 
-export default App
+  const addEmployee = (newEmployee) => {
+    setEmployees(prevEmployees => [...prevEmployees, newEmployee]);
+  };
+
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<EmployeeList employees={employees} />} />
+          <Route path="/add" element={<AddEmployee addEmployee={addEmployee} />} />
+        </Routes>
+      </div>
+    </Router>
+  );
+};
+
+export default App;
