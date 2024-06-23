692a1057b89dc5e3742e99cf09e308b064839846
aish <aishnikam31@gmail.com>
2024-06-23 13:01:31 +0530

    :iphone: styling the form

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,19 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './Navbar.css';
 
 const Navbar = () => {
+  const [click, setClick] = useState(false);
+
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
+
   return (
     <nav className="navbar">
       <div className="navbar-container">
-        <ul className="nav-menu">
+        <div className="menu-icon" onClick={handleClick}>
+          <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+        </div>
+        <ul className={click ? 'nav-menu active' : 'nav-menu'}>
           <li className="nav-item">
-            <Link to="/" className="nav-links">
+            <Link to="/" className="nav-links" onClick={closeMobileMenu}>
               Employee List
             </Link>
           </li>
           <li className="nav-item">
-            <Link to="/add" className="nav-links">
+            <Link to="/add" className="nav-links" onClick={closeMobileMenu}>
               Add Employee
             </Link>
           </li>
